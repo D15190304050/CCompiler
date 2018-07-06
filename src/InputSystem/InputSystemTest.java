ef5fdfd40392cfa1460104bfd1c8d91122e6f97e
鲍方舟 <shakuganno00@163.com>
2018-07-06 22:01:59 +0800

    Last updated: 2018.07.06 22:01

---
+++ src/InputSystem/InputSystemTest.java
@@ -0,0 +1,19 @@
+package InputSystem;
+
+/** The {@link InputSystemTest} class unit tests the {@link Input} class. */
+public class InputSystemTest
+{
+    private static void printWord()
+    {
+        
+    }
+
+    private static void runTest()
+    {
+        Input input = new Input();
+        input.newStandardInput();
+
+        input.markStart();
+
+    }
+}
